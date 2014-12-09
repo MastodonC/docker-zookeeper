af34353b2dabcc2ad65efd38ade06d9083593738
Neale Swinnerton <neale@isismanor.com>
2014-12-09 11:43:46 +0000

    First attempt at clustered (max 3 nodes) zookeeper.

--- start-zookeeper.sh
+++ start-zookeeper.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 LOG_CONFIG_FILE=/zookeeper/conf/log4j.properties
-
+CONFIG_FILE=/zookeeper/conf/zoo.cfg
 
 LOG_DIR="/logs/zookeeper/${HOSTNAME}"
 mkdir -p "${LOG_DIR}"
 
 DATA_DIR="/data/zookeeper/${HOSTNAME}"
+
 mkdir -p ${DATA_DIR}
 
 sed -i \
@@ -25,6 +26,18 @@ log4j.appender.LOGSTASH.ReconnectionDelay=30000
 EOF
 
 sed -e "s@dataDir=.*@dataDir=/data/zookeeper/${HOSTNAME}@" \
-    /zookeeper/conf/zoo_sample.cfg > /zookeeper/conf/zoo.cfg
+    /zookeeper/conf/zoo_sample.cfg > ${CONFIG_FILE}
+
+if [ -z "${ZK01_PORT_2888_TCP_ADDR}" ]; then
+    echo "server.1=${ZK01_PORT_2888_TCP_ADDR}:${ZK01_PORT_2888_TCP_PORT}:${ZK01_PORT_3888_TCP_PORT}" >> ${CONFIG_FILE}
+fi
+
+if [ -z "${ZK02_PORT_2888_TCP_ADDR}" ]; then
+    echo "server.2=${ZK02_PORT_2888_TCP_ADDR}:${ZK02_PORT_2888_TCP_PORT}:${ZK02_PORT_3888_TCP_PORT}" >> ${CONFIG_FILE}
+fi
+
+if [ -z "${ZK03_PORT_2888_TCP_ADDR}" ]; then
+    echo "server.3=${ZK03_PORT_2888_TCP_ADDR}:${ZK03_PORT_2888_TCP_PORT}:${ZK03_PORT_3888_TCP_PORT}" >> ${CONFIG_FILE}
+fi
 
 /zookeeper/bin/zkServer.sh start-foreground
